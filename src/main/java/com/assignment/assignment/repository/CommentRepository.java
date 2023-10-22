ecfa9722160b89d42466806b071298ed3da4f7db
AyushKshirsagar <ayuksh05@gmail.com>
2023-10-22 23:31:40 +0530

    problem with hikaripool

--- src/main/java/com/assignment/assignment/repository/CommentRepository.java
+++ src/main/java/com/assignment/assignment/repository/CommentRepository.java
@@ -2,6 +2,8 @@ package com.assignment.assignment.repository;
 
 import com.assignment.assignment.model.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CommentRepository extends JpaRepository<Comment , Integer> {
 }
