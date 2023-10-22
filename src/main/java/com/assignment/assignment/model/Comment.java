8f34d0cc05c3bf67ba2850118408aaf7ad083fca
AyushKshirsagar <ayuksh05@gmail.com>
2023-10-22 20:57:03 +0530

    model tables created

---
+++ src/main/java/com/assignment/assignment/model/Comment.java
@@ -0,0 +1,72 @@
+package com.assignment.assignment.model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "comment")
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "commentid")
+    private int commentId ;
+
+    @Column(name = "message")
+    private String message ;
+
+    // spring boot will automatically handle the conversion between timesstamp and LocalDateTime
+    @Column(name ="commentdatetime")
+    private LocalDateTime commentDateTime  ;
+
+    @Column(name ="postedbyuserid")
+    private int postedByUserId ;
+
+
+    // default constructor
+    public Comment() {
+    }
+
+
+    // parametrize constructor
+    public Comment(int commentId, String message, LocalDateTime commentDateTime, int postedByUserId) {
+        this.commentId = commentId;
+        this.message = message;
+        this.commentDateTime = commentDateTime;
+        this.postedByUserId = postedByUserId;
+    }
+
+    // getter and setter  methods
+    public int getCommentId() {
+        return commentId;
+    }
+
+    public void setCommentId(int commentId) {
+        this.commentId = commentId;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public LocalDateTime getCommentDateTime() {
+        return commentDateTime;
+    }
+
+    public void setCommentDateTime(LocalDateTime commentDateTime) {
+        this.commentDateTime = commentDateTime;
+    }
+
+    public int getPostedByUserId() {
+        return postedByUserId;
+    }
+
+    public void setPostedByUserId(int postedByUserId) {
+        this.postedByUserId = postedByUserId;
+    }
+}
---
+++ src/main/java/com/assignment/assignment/model/User.java
@@ -0,0 +1,52 @@
+package com.assignment.assignment.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "user")
+public class User {
+    // table columns userId , commentFrom , commentTo
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "userid")
+    private int userId ;
+    @Column(name = "commentfrom")
+    private String commentFrom ;
+    @Column(name = "commentto")
+    private String commentTo ;
+
+
+    // default constructor
+    public User() {
+    }
+
+    // parametrize constructor
+    public User(int userId, String commentFrom, String commentTo) {
+        this.userId = userId;
+        this.commentFrom = commentFrom;
+        this.commentTo = commentTo;
+    }
+
+
+    // getters method for userId , commentFrom , commentTo
+    public int getUserId() {
+        return userId;
+    }
+
+    public String getCommentFrom() {
+        return commentFrom;
+    }
+
+    public String getCommentTo() {
+        return commentTo;
+    }
+
+    // setter method for commentFrom and commentTo
+    public void setCommentFrom(String commentFrom) {
+        this.commentFrom = commentFrom;
+    }
+
+    public void setCommentTo(String commentTo) {
+        this.commentTo = commentTo;
+    }
+}
